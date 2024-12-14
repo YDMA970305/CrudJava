ab027c85baaed87c107000565983fab692b50f00
YDMA970305 <ymartineza@unicartagena.edu.co>
2024-12-13 22:52:49 -0500

    Se realiza la clase modelo de la tabla cliente

---
+++ src/Modelo/Cliente.java
@@ -0,0 +1,25 @@
+
+package Modelo;
+
+/**
+ *
+ * @author Yesid MArtinez
+ */
+public class Cliente {
+ 
+
+
+    private int dni_cliente;
+    private String nombre_cliente;
+    private String direccion_cliente;
+    private String email;
+    private String vehiculo_usado_matricula;
+    private String fecha; 
+
+    // Constructor
+    
+    
+    
+    
+
+}
